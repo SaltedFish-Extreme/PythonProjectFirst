1ea93486c62819d087a88d5129366bdfe46bc400
咸鱼至尊 <1738099872@qq.com>
2024-11-26 23:51:50 +0800

    更新身份证前六位对应省市区的json文件，重构获取方法，匹配直辖市和省直辖行政单位

--- 根据身份证前六位获取省市区.py
+++ 根据身份证前六位获取省市区.py
@@ -5,7 +5,10 @@ with open("身份证前六位对应的省市区.json", "r", encoding='UTF-8') as
     six = input("请输入身份证前6位：")
     for i in data.keys():
         if i.__contains__(six[0:2] + "0000"):
-            for j in data[i].keys():
+            for j, o in data[i].items():
+                if o == six:
+                    print(i.split(":")[0] + j)
+                    break
                 if j.__contains__(six[0:4] + "00"):
                     for k, v in (data[i][j]).items():
                         if v == six:
